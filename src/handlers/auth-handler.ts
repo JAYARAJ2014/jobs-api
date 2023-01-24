2831035d89aa296126f53ca498be494362cbf739
Jayaraj <jayaraj.sivadasan@gmail.com>
2023-01-23 23:40:40 -0500

    << Create Job and associatd View jobs with user >>

--- src/handlers/auth-handler.ts
+++ src/handlers/auth-handler.ts
@@ -21,7 +21,7 @@ class AuthHandler {
       }
     );
     console.log(`User Created:  `, user);
-    return res.status(StatusCodes.OK).json( {user: {userId:user._id,  name:username}, token: {token:token}});
+    return res.status(StatusCodes.OK).json( {user: {userId:user._id,  name:username}, token  :token});
   }
   public async login(req: Request, res: Response) {
     const { email, password } = req.body;
--- src/handlers/jobs-handler.ts
+++ src/handlers/jobs-handler.ts
@@ -2,18 +2,23 @@ import { Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError } from '../custom-errors/bad-request-error';
+import { IJob, Job } from '../models/job';
 
 class JobsHandler {
   public async getAllJobs(req: Request, res: Response) {
-    return res.status(200).json('All jobs');
+    const jobs = await Job.find({createdBy: req.user?.userId}).sort('createdAt');
+    return res.status(StatusCodes.OK).json({jobs, count: jobs.length});
   }
 
   public async getJob(req: Request, res: Response) {
-    return res.send('A job');
+    const {_id} = req.params;
+    const job = await Job.findOne({_id:_id})
+    return res.status(StatusCodes.OK).json(job);
   }
 
   public async createJob(req: Request, res: Response) {
-    return res.send('Created Job');
+    const job = await Job.create({ ...req.body as IJob , createdBy: req.user?.userId});
+    return res.status(StatusCodes.CREATED).json(job)
   }
   public async updateJob(req: Request, res: Response) {
     return res.send('Update  job');
